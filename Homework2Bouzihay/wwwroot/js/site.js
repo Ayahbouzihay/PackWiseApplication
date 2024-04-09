b227c78a1f73087dcca526b4294a04d2a68f9318
ajp00002 <ajp00002@mix.wvu.edu>
2024-04-09 18:41:17 -0400

    injected my second API inputTripdates

--- Homework2Bouzihay/wwwroot/js/site.js
+++ Homework2Bouzihay/wwwroot/js/site.js
@@ -10,6 +10,22 @@ async function displayTripCat(categoryName) {
     document.getElementById('TripName').style.visibility = "visible";
 }
 
+async function DateResults(date) {
+    const response = await fetch(`https://localhost:7270/api/InputTripDates/${date}`);
+    const data = await response.json();
+    document.getElementById('date').innerHTML = data[0].date;
+    document.getElementById('date').style.visibility = "visible";
+    DateWeatherResults(data[0].temperatureC);
+}
 
-
-
+// Define DateWeatherResults function separately
+async function DateWeatherResults(temperatureC) {
+    const response = await fetch(`https://localhost:7270/WeatherForecast/${temperatureC}`);
+    const data = await response.json();
+    document.getElementById('temperatureC').innerHTML = data[0].temperatureC;
+    document.getElementById('temperatureC').style.visibility = "visible";
+    document.getElementById('temperatureF').innerHTML = data[0].temperatureF;
+    document.getElementById('temperatureF').style.visibility = "visible";
+    document.getElementById('summary').innerHTML = data[0].summary;
+    document.getElementById('summary').style.visibility = "visible";
+}
